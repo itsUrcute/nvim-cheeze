eeb83258824dab7eea8f580802d3953b79841cc4
cheeze2000 <me@cheeze2000.net>
2022-01-01 18:57:31 +0800

    a

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,14 @@ return require("packer").startup(
 	function ()
 		use "wbthomason/packer.nvim"
 
+		use "cappyzawa/trim.nvim"
+
 		use "navarasu/onedark.nvim"
+
+		use {
+			"neovim/nvim-lspconfig",
+			"williamboman/nvim-lsp-installer"
+		}
 
 		use {
 			"nvim-treesitter/nvim-treesitter",
