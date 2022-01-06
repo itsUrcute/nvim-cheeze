085fff9a5d09c919a6f6e560b35bcc3da8d889d5
cheeze2000 <me@cheeze2000.net>
2022-01-06 16:16:51 +0000

    a

--- lua/config/neogit.lua
+++ lua/config/neogit.lua
@@ -3,8 +3,13 @@ local map = require("utils").map
 map("<Leader>gs", ":Neogit kind=split_above<CR>")
 
 require("neogit").setup({
+	disable_hint = true,
+	disable_commit_confirmation = true,
+	disable_insert_on_commit = false,
 	signs = {
 		section = { "+", "-" },
 		item = { "+", "-" }
 	}
 })
+
+require("neogit").config.use_magit_keybindings()
