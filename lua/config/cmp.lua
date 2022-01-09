b158d365b5bce86c71750b702c90d421a2697d33
cheeze2000 <me@cheeze2000.net>
2022-01-09 03:58:27 +0000

    a

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -3,12 +3,12 @@ local cmp = require("cmp")
 vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
+	preselect = cmp.PreselectMode.None,
 	snippet = {
 		expand = function (args)
 			vim.fn["vsnip#anonymous"](args.body)
 		end
 	},
-	mapping = {},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "vsnip" }
--- lua/config/onedark.lua
+++ lua/config/onedark.lua
@@ -3,3 +3,5 @@ require("onedark").setup({
 		comments = "none"
 	}
 })
+
+require("onedark").load()
