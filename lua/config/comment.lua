f5fb2e3c6b5c0b3706688a524945453ee94b2c5f
cheeze2000 <me@cheeze2000.net>
2022-06-15 19:59:09 +0800

    a

--- lua/config/comment.lua
+++ lua/config/comment.lua
@@ -1,5 +1,11 @@
+local map = require("utils").map
+local map_visual = require("utils").map_visual
+
 require("Comment").setup({
 	mappings = {
 		extra = false
 	}
 })
+
+map("<Leader>c", ":lua require('Comment.api').toggle_current_linewise()<CR>")
+map_visual("<Leader>c", ":lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>")
--- lua/utils.lua
+++ lua/utils.lua
@@ -4,4 +4,8 @@ M.map = function (lhs, rhs)
 	vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
 end
 
+M.map_visual = function (lhs, rhs)
+	vim.api.nvim_set_keymap("v", lhs, rhs, { noremap = true, silent = true })
+end
+
 return M
