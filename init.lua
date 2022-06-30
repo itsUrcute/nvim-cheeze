97e003a305755fa0911f5abdceb416e2a7d4660e
cheeze2000 <me@cheeze2000.net>
2022-07-01 01:11:01 +0800

    a

--- init.lua
+++ init.lua
@@ -52,10 +52,7 @@ require("packer").startup(
 
 		use "nvim-lualine/lualine.nvim"
 
-		use {
-			"neovim/nvim-lspconfig",
-			"williamboman/nvim-lsp-installer"
-		}
+		use "neovim/nvim-lspconfig"
 
 		use "kyazdani42/nvim-tree.lua"
 
--- lua/config/nvim-lsp.lua
+++ lua/config/nvim-lsp.lua
@@ -1,3 +1,5 @@
+local lspconfig = require("lspconfig")
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
 		virtual_text = {
@@ -9,17 +11,4 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	}
 )
 
-require("nvim-lsp-installer").on_server_ready(
-	function (server)
-		local opts = {}
-		opts.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-		if server.name == "eslint" then
-			opts = {
-				filetypes = { "javascript", "svelte", "vue" }
-			}
-		end
-
-		server:setup(opts)
-	end
-)
+lspconfig.hls.setup({})
